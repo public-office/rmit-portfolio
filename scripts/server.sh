9bd99deee2b2f771ad9ffce4e85c0d99bf825c51
Paul Mylecharane <hi@public-office.info>
2017-05-23 16:49:10 +1000

    added imagemagick into apache setup

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Basic Ubuntu/Apache setup
 # Update Ubuntu
-apt-get update;
-apt-get -y upgrade;
+sudo apt-get update;
+sudo apt-get -y upgrade;
 
 # Install Apache
-apt-get -y install apache2 php libapache2-mod-php7.0 php-mbstring php7.0-curl php7.0-zip php7.0-json php7.0-xml unzip wget;
+sudo apt-get -y install apache2 php libapache2-mod-php7.0 php-mbstring php7.0-curl php7.0-zip php7.0-json php7.0-xml imagemagick unzip wget;
 
 # Some other stuff
 chown -Rf www-data:www-data /var/www/html;
-a2enmod rewrite;
-service apache2 restart;
+sudo a2enmod rewrite;
+sudo service apache2 restart;
