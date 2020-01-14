f46dfe70c63f915025c855095617ba40af510e06
Paul Mylecharane <hi+github@public-office.info>
2020-01-14 15:11:13 +1100

    Update server.sh

--- scripts/server.sh
+++ scripts/server.sh
@@ -5,7 +5,7 @@ sudo apt-get update;
 sudo apt-get -y upgrade;
 
 # Install Apache
-sudo apt-get -y install apache2 php libapache2-mod-php7.0 php-mbstring php7.0-curl php7.0-zip php7.0-json php7.0-xml imagemagick unzip wget;
+sudo apt-get -y install apache2 php libapache2-mod-php7.4 php-mbstring php7.4-curl php7.4-zip php7.4-json php7.4-xml imagemagick unzip zip wget;
 
 # Some other stuff
 chown -Rf www-data:www-data /var/www/html;
